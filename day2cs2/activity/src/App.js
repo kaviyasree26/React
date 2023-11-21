70f7d78fd3de437dcbe99b9428a0fbcb55f62e10
kaviyasree26 <150135134+kaviyasree26@users.noreply.github.com>
2023-11-21 10:16:33 +0530

    Add files via upload

---
+++ day2cs2/activity/src/App.js
@@ -0,0 +1,12 @@
+
+function App() {
+  return (
+    <div >
+      
+      <h1 style={{color:'green'}}>Inline Style in JSX Example</h1>
+      <p style={{color:'darkblue' ,backgroundColor:'lightblue' ,padding:'10px',borderRadius: '5px' ,border:'1px solid blue' }} >This is a paragraph with inline styles applied</p>
+    </div>
+  );
+}
+
+export default App;
